7990c308a49057cd133e4431117d4c74eceaa061
Luis Andres Ubidia <ubs@Luiss-iMac.local>
2025-06-16 19:09:35 -0600

    bug fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,26 @@
 const forceDatabaseRefresh = false;
-
+import path from 'node:path';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
-
 app.use(express.json());
-app.use(routes);
+// Serves static files in the entire client's dist folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.use(routes);
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
+} else {
+  app.use(express.static('../client/dist'));
+  app.use(routes);
+}
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
